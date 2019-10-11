645b4fc369fc0c7fda154441a41857454cceee87
SuryaEka <56153778+suryaeka@users.noreply.github.com>
2019-10-11 16:40:02 +0700

    kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,11 @@
+nama = 'Surya Eka Kurnia'
+program = 'Gerak Lurus'
+
+print(f'program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print(f'jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan} m/s')
